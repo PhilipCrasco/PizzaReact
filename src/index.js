9ae46cf35a25c1d672908c6625367565d6420d03
philip crasco <philiplorenzcrasco@gmail.com>
2024-07-22 20:43:37 +0800

    rendering list

--- src/index.js
+++ src/index.js
@@ -72,7 +72,13 @@ function Menu() {
     <div>
       <main className="menu">
         <h2>Our Menu</h2>
-        <Pizza
+
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+        {/* <Pizza
           name="Pizza Spinaci"
           ingredients="Tomato, mozarella, spinach, and ricotta cheese"
           price={12}
@@ -86,7 +92,7 @@ function Menu() {
           price={10}
           photoName="pizzas/funghi.jpg"
           alt="Pizza Spinaci"
-        />
+        /> */}
       </main>
     </div>
   );
@@ -95,14 +101,14 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
